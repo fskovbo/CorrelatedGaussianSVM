bb09ffeaec7a2fb1000eddb784953ee1df30e584
fskovbo <f.skovbo@hotmail.com>
2017-11-10 15:58:27 +0100

    code is clean

--- include/PotentialList.h
+++ include/PotentialList.h
@@ -13,6 +13,7 @@ private:
   std::vector<PotentialStrategy*> list;
 public:
   PotentialList();
+  PotentialList(const std::initializer_list<PotentialStrategy*>& initlist);
   void addPotential(PotentialStrategy* pot);
   virtual double calculateExpectedPotential(mat& A1, mat& A2, vec& s1, vec& s2, mat& Binv, double detB);
   virtual double calculateExpectedPotential_noShift(mat& A1, mat& A2, mat& Binv, double detB);
--- mainfiles/TwoParticleSqueeze.cpp
+++ mainfiles/TwoParticleSqueeze.cpp
@@ -14,38 +14,36 @@
 
 int main() {
   clock_t begin = clock();
-
   arma_rng::set_seed_random();
 
-  vec masses = {1 , 1};
-  vec charges = {0 , 0};
+  vec masses            = {10 , 1};
+  vec charges           = {0 , 0};
+  auto TwoPart          = System(masses,charges);
 
-  System TwoPart = System(masses,charges);
+  auto Gauss            = SingleGaussPotential(TwoPart);
+  auto Trap             = TrapPotential(TwoPart);
+  PotentialList Vstrat  = {&Gauss, &Trap};
 
-  auto Gauss = SingleGaussPotential(TwoPart);
-  auto Trap = TrapPotential(TwoPart),
-
-  PotentialList Vstrat;
-  Vstrat.addPotential(&Gauss);
-  Vstrat.addPotential(&Trap);
-  auto elem = MatrixElements(TwoPart,Vstrat);
-  auto ansatz = Variational(TwoPart,elem);
+  auto elem             = MatrixElements(TwoPart,Vstrat);
+  auto ansatz           = Variational(TwoPart,elem);
 
 
+  vec startGuess        = 2.5*ones<vec>(3);
+  size_t Nvals          = 10;
+  vec bs                = logspace<vec>(-2,2,Nvals);
+  mat data              = zeros<mat>(Nvals,2);
+  double mui            = TwoPart.lambdamat(0,0);
 
-  size_t Nvals = 10;
-  vec bs = logspace<vec>(-2,2,Nvals);
-  mat data(Nvals,2);
-  double mui = TwoPart.lambdamat(0,0);
-  vec trapOsc;
   for (size_t i = 0; i < Nvals; i++) {
-    trapOsc = { pow(bs(i),-4) };
+    vec trapOsc   = { pow(bs(i),-4) };
+
     Trap.updateTrap(trapOsc);
     ansatz.initializeBasis(4);
-    vec res1 = ansatz.sweepStochastic(5,1e4,2.5*ones<vec>(3));
-    vec res2 = ansatz.sweepDeterministic(5,1e4);
-    data(i,0) = bs(i);
-    data(i,1) = res2(res2.n_rows-1) - 0.5*mui/bs(i)/bs(i);
+
+    vec res1      = ansatz.sweepStochastic(5,1e4,startGuess);
+    vec res2      = ansatz.sweepDeterministic(5,1e4);
+    data(i,0)     = bs(i);
+    data(i,1)     = res2(res2.n_rows-1) - 0.5*mui/bs(i)/bs(i);
   }
 
   clock_t end = clock();
