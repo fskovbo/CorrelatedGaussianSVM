ab4e02e891af1525b156b3c65254aed70b122023
fskovbo <f.skovbo@hotmail.com>
2018-02-25 10:31:14 +0100

    t

--- src/TrapPotential.cpp
+++ src/TrapPotential.cpp
@@ -13,7 +13,7 @@ TrapPotential::TrapPotential(System& sys, vec trapLength)
 
 void TrapPotential::updateTrap(vec trapLength){
   mat trapmat = diagmat(repmat( pow(trapLength,-4) ,n,1));
-  Omega       = zeros(lambdamat);
+  Omega       = zeros<mat>(lambdamat.n_rows,lambdamat.n_rows);
   Omega       = 0.5*pow(lambdamat,-1)%trapmat;
   gsEnergy    = 1.5*trace(lambdamat%diagmat(repmat( pow(trapLength,-2) ,n,1)))/De;
 }
